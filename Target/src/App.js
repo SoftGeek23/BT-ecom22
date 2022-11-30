56807621243e531731b36d3a6ae7c62417e7b0eb
Ryanho12123 <ryanho12123@gmail.com>
2022-11-29 19:36:15 -0600

    Target

---
+++ Target/src/App.js
@@ -0,0 +1,55 @@
+import './App.css';
+import Axios from "axios";
+import React, { useEffect, useState } from "react";
+
+export default function App(){
+const axios = require('axios');
+const list = [];
+
+
+// set up the request parameters
+const params = {
+  api_key: "C03F56CB00FE42F3A357ADB3EC1C24A3",
+  type: "search",
+  search_term: "highlighter pens",
+  sort_by: "best_seller"
+}
+
+// make the http GET request to RedCircle API
+Axios.get('https://api.redcircleapi.com/request', { params })
+  .then(response => {
+
+
+    // print the JSON response from RedCircle API
+    console.log(JSON.stringify(response.data, 0, 2));
+    const results = response.data.search_results;
+    console.log(results);
+
+    for(var i = 0; i < results.length; i++) {
+      console.log("Price: " + results[i].offers.primary.price);
+      console.log("Rating: " + results[i].product.rating);
+      const delivery = results[i].fulfillment.shipping;
+      if(delivery){
+        console.log("Shipping days: " + results[i].fulfillment.shipping_days);
+      }  
+      else {
+        console.log("No delivery date");
+      }
+    }
+
+  }).catch(error => {
+    // catch and print the error
+    console.log(error);
+  })
+
+
+
+
+
+  return (
+    <div className="App">
+      <h1>API Posts</h1>
+
+    </div>
+  );
+}

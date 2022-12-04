78eedce19b65b396124d02573a14f515a467bdcb
caseycharleston <caseycharleston@gmail.com>
2022-12-04 16:28:43 -0600

    front end HTML and CSS finalization + some new js

--- popup.js
+++ popup.js
@@ -6,6 +6,13 @@ document.addEventListener('DOMContentLoaded', function () {
     const settings = document.querySelector('#settings');
     settings.addEventListener('click', (e) => {
     })
+
+    var link = "https://www.amazon.com/Apple-AirPods-3rd-Generation-Renewed/dp/B09M94KXPN/ref=sr_1_4?crid=3R6FEV5ND7N6&keywords=apple+airpods+3rd+generation&qid=1670192526&sprefix=apple+airpods+%2Caps%2C102&sr=8-4";
+    const image = document.querySelector('#product_image');
+    image.addEventListener('click', (e) => {
+        window.open(link);
+    })
+
     const companies = document.querySelectorAll('.company');
     companies.forEach(company => {
         company.addEventListener('click', function toggleHighlight(event) {
